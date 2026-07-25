a69202fc81b551d53c93bd632b6a8af62024b99e
Nyan Min Htet <nyanminhtet2304@gmail.com>
2026-07-25 21:08:02 +0700

    Upload Chapter 1 Exercises

---
+++ Chapter_1/ch1_2.py
@@ -0,0 +1,6 @@
+x = 5                  # Create a variable named x and store the number 2 in the variable x.
+print("x =", x)         # Display the value of x
+# Add 3 to the value stored in the variable x and then store it back in the variable x
+# (replace the original data).
+x = x + 3
+print("new x =", x)     # Display the value of x.
---
+++ Chapter_1/ch1_3.py
@@ -0,0 +1,10 @@
+# lambda = 79         # Create a variable named lambda and store the number 79 in the variable lambda.
+# print("lambda =",lambda)    # Display the value of lambda.
+
+# Output:
+# File "/Users/nyanminhtet/Desktop/KMITL/Computer-Programming/Lecture 1/ch1_3.py", line 1
+#    lambda=79         # Create a variable named lambda and store the number 79 in the variable lambda.
+#          ^
+# SyntaxError: invalid syntax
+
+print("lambda = 79")
